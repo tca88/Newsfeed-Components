4a688834b3ae95ffe7e2bdc1748a8366191492a3
tca88 <trishnaamin@gmail.com>
2019-03-06 15:57:48 -0500

    Select all classes named .article and assign that value to a new variable named articles. Create a new instance of Article in chained forEach by passing in each article as a parameter to the Article class.

--- Article/Article.js
+++ Article/Article.js
@@ -20,10 +20,14 @@ class Article {
 
 /* START HERE: 
 
-- Select all classes named ".article" and assign that value to the articles variable.  
+- Select all classes named ".article" and assign that value to the articles variable. 
+
 
 - With your selection in place, now chain .forEach() on to the articles variable to iterate over the articles NodeList and create a new instance of Article by passing in each article as a parameter to the Article class.
 
 */
 
-let articles;
+let articles = document.querySelectorAll('.article'); 
+articles.forEach(function(article){
+  return new Article(article);
+});
