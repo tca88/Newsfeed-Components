c5a6b8fcfc77789bd6ffbf0e90b74bc53a06db23
tca88 <trishnaamin@gmail.com>
2019-03-06 15:51:33 -0500

    Add click event that toggles menu from hidden to visible and vice versa.

--- Menu/Menu.js
+++ Menu/Menu.js
@@ -1,10 +1,13 @@
-
-const toggleMenu = () => {
-  // Toggle the "menu--open" class on your menu refence. 
-}
-
 // Start Here: Create a reference to the ".menu" class
 const menu = document.querySelector('.menu');
+// console.log(menu);
 // create a reference to the ".menu-button" class
 const menuButton = document.querySelector('.menu-button');
+// console.log(menuButton);
+
+const toggleMenu = () => {
+  menu.classList.toggle('menu--open');
+  // console.log(menu);
+}
 // Using your menuButton reference, add a click handler that calls toggleMenu
+menuButton.addEventListener('click', () => toggleMenu());
